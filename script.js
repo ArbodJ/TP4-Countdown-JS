ae9139f5bc606861d03f941e5e24f80b442bcaef
Jerome <issa@A.J.Issa>
2025-01-05 13:57:11 +0100

    Dev JS

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("test");
+const input = document.getElementById("choice");
+const form = document.getElementById("form");
+const btn = document.getElementById("start");
+const counter = document.getElementById("countdownDisplay");
+let totalSeconds = 0;
+let interval;
+
+// logique de compte a rebours
+function countdown() {
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  counter.textContent = minutes + ":" + seconds;
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    counter.textContent = "FIN";
+    clearInterval(interval);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  clearInterval(interval);
+  if (isNaN(totalSeconds)) {
+    alert("N'entrez que des chifrres");
+  } else {
+    totalSeconds = input.value * 60;
+    input.value = "";
+    interval = setInterval(countdown, 100);
+  }
+});
+console.log(totalSeconds);
